545ce0fb8fb5df067f60a556a18786ce8fa5342d
TheFox1998 <MZrG3gzjLBUA>
2021-05-12 22:40:17 +0300

    feat(all): __init__

--- ksysha/app.py
+++
@@ -1,5 +0,0 @@
-
-if __name__ == "__main__":
-    from handlers import dp
-    from aiogram import executor
-    executor.start_polling(dp)
--- ksysha/states/register.py
+++
@@ -1,6 +0,0 @@
-from aiogram.dispatcher.filters.state import StatesGroup, State
-
-
-class Main(StatesGroup):
-    start_name = State()
-    start_gender = State()
